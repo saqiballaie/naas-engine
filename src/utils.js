5f7743aaf2164f61381f55c686a17576c8e8b4da
saqiballaie <saqib.parvaze@gmail.com>
2026-03-26 15:32:13 +0530

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+export function escapeHTML(str) {
+    if (!str) return '';
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
